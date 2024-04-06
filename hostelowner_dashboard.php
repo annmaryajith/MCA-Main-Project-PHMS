8d9b7199efa80a71508d9ca6e68579dc76a2ce35
annmaryajith <118360733+annmaryajith@users.noreply.github.com>
2024-04-06 09:14:53 +0530

    Add files via upload

--- hostelowner_dashboard.php
+++ hostelowner_dashboard.php
@@ -47,10 +47,13 @@
             <h2>PHMS | Hostel Owner</h2>
             <ul>
                 <li><a href="hostelowner_dashboard.php">Home</a></li>
-                <li><a href="addhostelroom.php">Add Rooms</a></li>
+                <li><a href="owner_interface.php">Add Rooms</a></li>
                 <li><a href="viewhostelroom.php">View Rooms</a></li>
                 <!-- <li><a href="hostelroomupdate.php">Update room</a></li> -->
-                <li><a href="hostelownerupdate.php">Update</a></li>
+                <li><a href="add_hostel_details.php">Add Hostel details</a></li>
+                <li><a href="hosteldetailsupdate.php">Update Hostel details</a></li>
+                <li><a href="add_hostelowner_details.php">Add contact details</a></li>
+                <li><a href="hostelownerupdate.php">Update profile</a></li>
                 <!-- <li><a href="manage_tenants.php">Manage Tenants</a></li>
                 <li><a href="reports.php">Reports</a></li> -->
                 <li><a href="logout.php" class="logout">Logout</a></li>
